98609ca65d61aff17359c8cbcbf5d0d7754677f9
Eli Mallon <ekmallon@gmail.com>
2013-06-05 07:47:49 -0700

    PolyPath had tests backwards--positive Y is down.

--- tests/unit/PolyPath.js
+++ tests/unit/PolyPath.js
@@ -18,8 +18,8 @@ goog.provide("Tests.Unit.PolyPath");
         'PolyPath bottom mode' : function() {
             var points = [{x: 0, y: 0}, {x: 100, y: 0}, {x: 200, y: 100}, {x: 200, y: 200}, {x: 100, y: 100}, {x: 0, y: 100}];
             var poly = new PAE.PolyPath({points: points, mode: 'bottom'});
-            this.assertEquals(poly.getPoint({x: 50, y: 50}),   {x: 50, y: 0},   'Point on straight line works');
-            this.assertEquals(poly.getPoint({x: 150, y: 100}), {x: 150, y: 50}, 'Point on diagonal surface works');
+            this.assertEquals(poly.getPoint({x: 50, y: 50}),   {x: 50, y: 100},   'Point on straight line works');
+            this.assertEquals(poly.getPoint({x: 150, y: 100}), {x: 150, y: 150}, 'Point on diagonal surface works');
             this.assertEquals(poly.getPoint({x: 300, y: 300}), null,            'Point outside of object yields null');
             this.done();
         }
